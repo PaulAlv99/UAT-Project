3935907d5a65ff4fcf83cdc0ffd0e2dfe4c99920
PaulAlv99 <2120722@student.uma.pt>
2025-05-05 16:50:06 +0200

    configs

---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors()); // Allow requests from React Native
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from backend!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
